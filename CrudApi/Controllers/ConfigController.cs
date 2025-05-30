ff4b0630c25e386c2795221a9563b06a1f88dab1
VarlorsInnovations <varlorsinnovations@gmail.com>
2025-05-31 00:06:11 +0200

    starting with a config api

---
+++ CrudApi/Controllers/ConfigController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConfigController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAsync(CancellationToken cancellationToken)
+    {
+        
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(AiConfig config, CancellationToken cancellationToken)
+    {
+        
+    }
+    
+}
+
+public class GeneralConfig
+{
+    public string RootPath { get; set; } 
+}
+
+public enum AiPlatform
+{
+    Ollama,
+    ChatGpt
+}
+
+public class AiConfig
+{
+    public AiPlatform Platform { get; set; } = AiPlatform.Ollama;
+    
+    public string ApiUrl { get; set; } = "http://localhost:11434/";
+
+    public string? ApiKey { get; set; }
+}
